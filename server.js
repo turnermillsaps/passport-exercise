313884d367868db132d9cbcc088530bbdb35a3b5
Turner Millsaps <turnermillsaps.plp@gmail.com>
2020-05-11 19:52:23 -0400

    Created views folder with simple test login.ejs file; tested successfully

--- server.js
+++ server.js
@@ -3,11 +3,21 @@ const Sequelize = require('sequelize');
 
 const app = express();
 
-app.set('views-engine','ejs');
+app.set('view engine','ejs');
 app.set('views','views');
 
+// Test route to ensure express is configured properly
+/* 
 app.get('/ping', (req, res) => {
     res.send('pong');
+}) 
+*/
+
+// Initial route to login page
+app.get('/', (req, res) => {
+    res.render('login', {
+        name: 'Hello World!'
+    })
 })
 
 app.listen(3000, () => {
